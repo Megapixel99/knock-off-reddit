51d88f6171b9e2c7e771c863c1f401d555404cc1
Seth Wheeler <wheelersc@cox.net>
2021-02-28 18:47:38 -0800

    initial commit (end of 1st session)

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { dbConn, env, session } = require('./Functions');
+
+const app = express();
+
+dbConn.connect();
+
+app.set('json spaces', 2);
+app.use(require('helmet')());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.use(session);
+
+app.get('/ping', (req, res) => {
+  res.status(200).send('pong');
+});
+
+const listener = app.listen(3000, () => {
+  console.log('Application is live on port 3000');
+});
